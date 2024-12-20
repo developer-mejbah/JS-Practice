16c54fc03927cc5ec198fe3942f3544cfe334e75
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-21 01:24:54 +0600

    Started Let and Const

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log(2003);
+console.log("Mejbah Morsalin");
+
+// Let And Const Practice
+let potato = 60;
+ potato= 70;
+ potato = 80;
+console.log(potato);
+
+const fixedPrice = 90;
+console.log(fixedPrice);
