5cd07c36454dbb0c1f247b6664fc30044a55626d
mejbah99 <mejbahmorsalin521@gmail.com>
2024-12-21 01:36:21 +0600

    Done

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ console.log(potato);
 
 const fixedPrice = 90;
 console.log(fixedPrice);
+
+let Rice = 50;
+ rice= 70;
+ rice = 90;
+console.log(potato);
+
+const price = 130;
+console.log(price);
+console.log(typeof 130);
+console.log(typeof 'Mejbah Morsalin');
